aba7d22b40ca4a738f8dc06b546f2e861928260f
GedFenir <brice57200@live.Fr>
2018-10-05 16:01:52 +0200

    Login js

--- dashboard_server/dashboard_server/Resources/static/js/login.js
+++ dashboard_server/dashboard_server/Resources/static/js/login.js
@@ -1,3 +1,19 @@
-function login() {
-    
-}
+var button = document.getElementById("login_button");
+
+
+$("#login_button").click(function () {
+    var username = $('#login_username').val();
+    var pass1 = $('#login_pass1').val();
+
+  
+
+    var formData = username + ':' + pass1;
+    console.log(formData);
+    $.post("http://10.18.207.77:8080/?/login", {
+        data: formData
+    })
+});
+
+$("#login_form").submit(function (e) {
+    e.preventDefault();
+});
