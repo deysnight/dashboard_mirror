ba81c9f55c0ae86e9360c72902d88ae630d64e6f
Unknown <louison.harizi@epitech.eu>
2018-10-05 16:10:46 +0200

    PUTAIN

--- dashboard_server/dashboard_server/serialize_data.cs
+++ dashboard_server/dashboard_server/serialize_data.cs
@@ -25,13 +25,14 @@ namespace WebServer
 
         public static string Process_login(HttpListenerRequest request)
         {
-           
-            return ("bite");
-        }
 
-        public static string Process_signup(HttpListenerRequest request)
-        {
-            List<Users> all_users = new List<Users>(); // list a recup de fichier json deja existant
+            string path = Directory.GetCurrentDirectory() + "\\users.json";
+            List<Users> all_users = new List<Users>(); // list a recup de fichier json deja existant 
+            if (File.Exists(path))
+            {
+                string temp = File.ReadAllText(path);
+                all_users = JsonConvert.DeserializeObject<List<Users>>(temp);
+            }
 
             Stream body = request.InputStream;
             Encoding encoding = request.ContentEncoding;
@@ -39,10 +40,66 @@ namespace WebServer
             string s = reader.ReadToEnd();
             body.Close();
             reader.Close();
+
+            Console.WriteLine(s);
+
+            s = s.Replace("data=", "");
+            s = s.Replace("%3A", ":");
+
+            string[] words = s.Split(':');
+
+            //parcour voir si user exist 
+            foreach (Users u in all_users)
+            {
+                if (u.login == words[0] && u.password == words[1])
+                {
+                    Console.WriteLine("OK LOGIN");
+                    return ("OK LOGIN");
+                }
+                else if (u.login == words[0] && u.password != words[1])
+                {
+                    Console.WriteLine("KO PASS");
+                    return ("KO PASS");
+                }
+            }
+            Console.WriteLine("KO LOGIN");
+            return ("KO LOGIN");
+        }
+
+        public static string Process_signup(HttpListenerRequest request)
+        {
+            string path = Directory.GetCurrentDirectory() + "\\users.json";
+            List<Users> all_users = new List<Users>(); // list a recup de fichier json deja existant 
+            if (File.Exists(path))
+            {
+                string temp = File.ReadAllText(path);
+                all_users = JsonConvert.DeserializeObject<List<Users>>(temp);
+            }
+
+            Stream body = request.InputStream;
+            Encoding encoding = request.ContentEncoding;
+            StreamReader reader = new StreamReader(body, encoding);
+            string s = reader.ReadToEnd();
+            body.Close();
+            reader.Close();
+            s = s.Replace("data=", "");
             s = s.Replace("%3A", ":");
             s = s.Replace("%40", "@");
 
             string[] words = s.Split(':');
+
+            //parcour voir si user exist pas deja 
+            foreach (Users u in all_users)
+            {
+                if (u.login == words[0])
+                {
+                    return ("KO LOGIN");
+                }
+                else if (u.mail == words[1])
+                {
+                    return ("KO MAIL");
+                }
+            }
 
             Users user = new Users();
             user.login = words[0];
@@ -53,14 +110,11 @@ namespace WebServer
             all_users.Add(user);
 
             string output = JsonConvert.SerializeObject(all_users);
+            TextWriter tw = new StreamWriter(path);
+            tw.WriteLine(output);
+            tw.Close();
 
-
-
-            Console.WriteLine(words[0]);
-            Console.WriteLine(words[1]);
-            Console.WriteLine(words[2]);
-
-            return (s);
+            return ("OK USER REGISTER");
         }
     }
 }
