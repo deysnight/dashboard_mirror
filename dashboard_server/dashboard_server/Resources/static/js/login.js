f8da33486b80665314862b203fbb7deea711926b
GedFenir <brice57200@live.Fr>
2018-10-06 18:49:35 +0200

    Callback en place + tout les cas d'erreurs sur les form

--- dashboard_server/dashboard_server/Resources/static/js/login.js
+++ dashboard_server/dashboard_server/Resources/static/js/login.js
@@ -1,18 +1,54 @@
 var button = document.getElementById("login_button");
+var cpt_field = 0;
+var cpt_login_error = 0;
 
 
 $("#login_button").click(function () {
     var username = $('#login_username').val();
     var pass1 = $('#login_pass1').val();
 
-  
+    
+    if (username == "" ||  pass1 == "") {
+        if (cpt_field < 1) {
+            input = $('<p class="error_fade" id="error_field">Tout les champs doivent être remplis.</p>');
+            input.insertAfter('.pass1_div');
+            input.hide();
+            input.fadeIn(1000);
+            cpt_field++;
+        }
+        return false;
+    }
+    else {
+        $("#error_field").remove();
+        cpt_field = 0
+    }
+
 
     var formData = username + ':' + pass1;
-    console.log(formData);
-    $.post("http://10.18.207.77:8080/?/login", {
-        data: formData
-    })
-});
+    $.ajax(
+        {
+            url: "http://localhost:8080/?/login",
+            type: "post",
+            async: false,
+            data: formData,
+            success: function(response){
+                if (response == "OK LOGIN") {
+                    // create cookie
+                    $("#error_login").remove();
+                    cpt_login_error = 0;
+                }
+                else {
+                    if (cpt_login_error < 1) {
+                        input = $('<p class="error_fade" id="error_login">Vos données de connexion sont incorrectes.</p>');
+                        input.insertAfter('.pass1_div');
+                        input.hide();
+                        input.fadeIn(1000);
+                        cpt_login_error++;
+                    }
+                }
+            }
+        })
+    });
 
 $("#login_form").submit(function (e) {
     e.preventDefault();
--- dashboard_server/dashboard_server/Resources/static/js/signup.js
+++ dashboard_server/dashboard_server/Resources/static/js/signup.js
@@ -1,7 +1,11 @@
 var button = document.getElementById("signup_button");
+var cpt_field = 0;
 var cpt_user_len = 0;
 var cpt_pass_len = 0;
 var cpt_pass_same = 0;
+
+var error_signup_login = 0;
+var error_signup_mail = 0;
 
 
 $("#signup_button").click(function () {
@@ -11,6 +15,30 @@ $("#signup_button").click(function () {
     var pass2 = $('#signup_pass2').val();
 
     if (pass1.length < 8 || pass1 != pass2 || username.length < 6) {
+        if (username == "" || email == "" || pass1 == "" || pass2 == "") {
+            if (cpt_field < 1) {
+                input = $('<p class="error_fade" id="error_field">Tout les champs doivent être remplis.</p>');
+                input.insertAfter('.pass2_div');
+                input.hide();
+                input.fadeIn(1000);
+                cpt_field++;
+            }
+            $("#error_text_same").remove();
+            $("#error_text_len").remove();
+            $("#error_user_len").remove();
+            $("#error_signup_login").remove();
+            $("#error_signup_mail").remove();
+            cpt_user_len = 0;
+            cpt_pass_len = 0;
+            cpt_pass_same = 0;
+            error_signup_login = 0;
+            error_signup_mail = 0;
+            return false;
+        }
+        else {
+            $("#error_field").remove();
+            cpt_field = 0
+        }
         if (username.length < 6) {
             if (cpt_user_len < 1) {
                 input = $('<p class="error_fade" id="error_user_len">Votre nom de compte est trop court (6 lettres minimum).</p>');
@@ -52,11 +80,59 @@ $("#signup_button").click(function () {
         }
         return false;
     }
+    else {
+        $("#error_text_same").remove();
+        $("#error_text_len").remove();
+        $("#error_user_len").remove();
+        $("#error_field").remove();
+    }
 
     var formData = username + ':' + email + ':' + pass1;
-    console.log(formData);
-    $.post("http://10.18.207.77:8080/?/signup", {
-        data: formData
+
+   $.ajax(
+    {
+        url: "http://localhost:8080/?/signup",
+        type: "post",
+        async: false,
+        data: formData,
+        success: function(response){
+            if (response == "OK USER REGISTER") {
+                $("#error_signup_login").remove();
+                error_signup_login = 0;
+                $("#error_signup_mail").remove();
+                error_signup_mail = 0;
+            }
+            else {
+                if (response == "KO LOGIN") {
+                    if (error_signup_login < 1) {
+                       input = $('<p class="error_fade" id="error_signup_login">Ce nom de compte est déjà utilisé.</p>');
+                       input.insertAfter('.pass2_div');
+                       input.hide();
+                       input.fadeIn(1000);
+                       error_signup_login++;
+                    }
+                }
+                else {
+                    $("#error_signup_login").remove();
+                    error_signup_login = 0
+                }    
+                
+
+                if (response == "KO MAIL") {
+                    if (error_signup_mail < 1) {
+                        input = $('<p class="error_fade" id="error_signup_mail">Cette adresse email est déjà utilisée.</p>');
+                        input.insertAfter('.pass2_div');
+                        input.hide();
+                        input.fadeIn(1000);
+                        error_signup_mail++;
+                    }
+                }
+                else {
+                    $("#error_signup_mail").remove();
+                    error_signup_mail = 0
+                }      
+            }
+        }
     })
 });
 
