8fb00f3ba347099e5773e65e5fb29bdda6ff703b
GedFenir <brice57200@live.Fr>
2018-10-08 16:39:40 +0200

    Checkmark + dashboard

--- dashboard_server/dashboard_server/Resources/static/js/dashboard.js
+++ dashboard_server/dashboard_server/Resources/static/js/dashboard.js
@@ -12,11 +12,62 @@ closebutton.onclick = function() {
     modal.style.display = "none";
 };
 
-window.onclick = function(event) {
-    if (event.target === modal) {
-        modal.style.display = "none";
-    }
-};
+
+$('#checkmark_meteo').click(function () {
+
+    var $checks = $('input:checkbox[name=meteo_check]');
+
+     $checks.prop("checked", !$checks.is(":checked"));
+});
+
+
+$('#checkmark_steam').click(function () {
+
+    var $checks = $('input:checkbox[name=steam_check]');
+
+     $checks.prop("checked", !$checks.is(":checked"));
+});
+
+
+$('#checkmark_twitch').click(function () {
+
+    var $checks = $('input:checkbox[name=twitch_check]');
+
+     $checks.prop("checked", !$checks.is(":checked"));
+});
+
+
+$('#checkmark_crypto').click(function () {
+
+    var $checks = $('input:checkbox[name=crypto_check]');
+
+     $checks.prop("checked", !$checks.is(":checked"));
+});
+
+
+$('#checkmark_youtube').click(function () {
+
+    var $checks = $('input:checkbox[name=youtube_check]');
+
+     $checks.prop("checked", !$checks.is(":checked"));
+});
+
+
+$('#checkmark_starcraft').click(function () {
+
+    var $checks = $('input:checkbox[name=starcraft_check]');
+
+     $checks.prop("checked", !$checks.is(":checked"));
+});
+
+
+$('#checkmark_epitech').click(function () {
+
+    var $checks = $('input:checkbox[name=epitech_check]');
+
+     $checks.prop("checked", !$checks.is(":checked"));
+});
+
 
 document.getElementById('meteo_service').onclick = function(event) {
     document.getElementById('meteo_intel').style.display = "block";
@@ -87,3 +138,8 @@ document.getElementById('epitech_service').onclick = function(event) {
     document.getElementById('starcraft_intel').style.display = "none";
     document.getElementById('epitech_intel').style.display = "block";
 }
+
+$(function() {
+    $( "#sortable" ).sortable();
+    $( "#sortable" ).disableSelection();
+  });
