0d520f83d06a11d32475d1cf11d7fcffd1a885d6
Unknown <louison.harizi@epitech.eu>
2018-10-18 16:17:38 +0200

    FIX ABOUT.JSON ET UN AUTRE TRUC J'AI OUBLIER CE QUE C'ETAIT
    HA OUI C'ETAIT LA ROUTE DASHBOARD

--- dashboard/dashboard/Controllers/PagesController.cs
+++ dashboard/dashboard/Controllers/PagesController.cs
@@ -6,6 +6,7 @@ using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
@@ -15,6 +16,8 @@ namespace dashboard.Controllers
     public class HomeController : ControllerBase
     {
         [Route("")]
+        [Route("home")]
+        [Route("home/dashboard")]
         [HttpGet]
         public ActionResult<string> Dashboard()
         {
@@ -38,6 +41,23 @@ namespace dashboard.Controllers
         public ActionResult<string> Notfound()
         {
             return Program.Retrive_file("notfound.html", "text/html", "html");
+        }
+
+        [Route("about.json")]
+        [HttpGet]
+        public ActionResult<string> About()
+        {
+            string filePath = Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot\");
+            filePath += "\\about.json";
+            dynamic about = JsonConvert.DeserializeObject(System.IO.File.ReadAllText(filePath));
+
+            var remoteIpAddress = HttpContext.Features.Get<IHttpConnectionFeature>()?.RemoteIpAddress.ToString();
+            about.client.host = remoteIpAddress;
+
+            TimeSpan t = DateTime.UtcNow - new DateTime(1970, 1, 1);
+            int secondsSinceEpoch = (int)t.TotalSeconds;
+            about.server.current_time = secondsSinceEpoch;
+            return JsonConvert.SerializeObject(about);
         }
     }
 
